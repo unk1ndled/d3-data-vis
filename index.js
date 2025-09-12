3de243988e2e0bf6d663950ccbfec15effec7044
unk1ndled <amine.games2002@gmail.com>
2025-09-12 16:29:37 +0200

    navigation

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import * as d3 from "d3";
+// import * as d3 from "d3";
 
-const svg = d3.select("svg");
-svg
-  .append("rect")
-  .attr("x", 50)
-  .attr("y", 50)
-  .attr("width", 200)
-  .attr("height", 100)
-  .attr("fill", "tomato");
+// const svg = d3.select("svg");
+// svg
+//   .append("rect")
+//   .attr("x", 50)
+//   .attr("y", 50)
+//   .attr("width", 200)
+//   .attr("height", 100)
+//   .attr("fill", "tomato");
