5d9bce7e3e7abda6c23a596f2a4d2e529bebaaf8
unk1ndled <amine.games2002@gmail.com>
2025-10-02 22:44:18 +0200

    fix initial value

--- pages/steam/steam.js
+++ pages/steam/steam.js
@@ -183,7 +183,7 @@ function setupControls() {
     slider.max = maxYear;
   });
 
-  CONTROLS.yearFilter.value = 2010;
+  CONTROLS.yearFilter.value = 2012;
 
   CONTROLS.yearRangeMin.value = minYear;
   CONTROLS.yearRangeMax.value = maxYear;
